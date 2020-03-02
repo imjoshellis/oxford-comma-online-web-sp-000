73f66627ce06b994e57a16373e38270b180288a8
Josh Ellis <joshe523@gmail.com>
2020-03-02 22:40:00 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -4,7 +4,7 @@ def oxford_comma(array)
   elsif array.size == 2
     return "#{array[0]} and #{array[1]}"
   elsif array.size > 2
-    array[array.size] = "and #{array.last}"
+    array[array.size] = "and #{array[array.size]}"
     array.join(', ')
     return array
   end
