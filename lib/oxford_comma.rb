8368c63617ef88dc055642c7ccac1ff1d5a3d09b
Josh Ellis <joshe523@gmail.com>
2020-03-02 22:39:35 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,10 +1,9 @@
 def oxford_comma(array)
-  case array
-  when array.size == 1
+  if array.size == 1
     return array[0]
-  when array.size == 2
+  elsif array.size == 2
     return "#{array[0]} and #{array[1]}"
-  when array.size > 2
+  elsif array.size > 2
     array[array.size] = "and #{array.last}"
     array.join(', ')
     return array
